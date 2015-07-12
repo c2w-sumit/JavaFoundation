40b831116534601588cd895453704e73dcf48a42
clubs2winadmin <clubs2win@outlook.com>
2015-07-12 16:09:34 -0700

    Initial  Check-in of Session6 codebase

---
+++ Session6HW/src/com/clubs2win/Main.java
@@ -0,0 +1,10 @@
+package com.clubs2win;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // This is a test of Git and GitHub Version Control Integration
+        System.out.println("I am Git and GitHub enabled!");
+
+    }
+}
