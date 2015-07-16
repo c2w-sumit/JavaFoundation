5988cf3c465059b7d23a21669207113e2b033379
clubs2winadmin <clubs2win@outlook.com>
2015-07-16 15:45:31 -0700

    Session 7 HW Template with comments

--- Session7HW/src/com/clubs2win/session7/Main.java
+++ Session7HW/src/com/clubs2win/session7/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	/* Session 7 Coding HW *******************************
         Coding Assignment: Due @ 10pm  Friday 7/17
-        WarmUp-2:  countXX, noTriples
+        WarmUp-2:  countXX
         Logic-2: makeChocolate
         You are encouraged to explore the “Code Help and Videos’ on codingbat.com
         Once you get the logic working on codingbat.com, please port your programs to intelliJ.
@@ -16,5 +16,8 @@ public class Main {
         Turn-In process  Simply push your changes to GitHub from IntelliJ.
 
       ****************************************/
+
+
+
     }
 }
