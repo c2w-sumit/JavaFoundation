f55ca70797398b046dd133985472236cd4f3e45a
c2w-sumit <sumit@chawlas.net>
2015-07-21 10:47:58 -0700

    updated session8

--- Session8HW/src/com/clubs2win/session8/Main.java
+++ Session8HW/src/com/clubs2win/session8/Main.java
@@ -4,5 +4,32 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        isPalindrome(357);
     }
+
+    public static boolean isPalindrome(int number)
+    {
+
+        //make a copy so we can reverse it without destroying the original
+        int inputNumber = number;
+        int reverse = 0;
+        boolean returnValue = false;
+
+        while (inputNumber != 0)
+        {
+            int remainder = inputNumber % 10;
+            reverse = reverse * 10 + remainder;
+            inputNumber = inputNumber / 10;
+        }
+
+        // if original and reverse of number is equal
+        // then it is a palindrome
+        if (number == reverse)
+        {
+            returnValue = true;
+        }
+        return returnValue;
+    }
+
 }
