a07e81609b11d6cbd3083fb8a0de1a2c42a468f7
c2w-sumit <sumit@chawlas.net>
2015-07-21 15:15:44 -0700

    Sumit's Session 9 update

---
+++ Session9HW/src/com/clubs2win/session9/Dog.java
@@ -0,0 +1,15 @@
+package com.clubs2win.session9;
+
+/**
+ * Created by sumitchawla on 7/20/15.
+ */
+public class Dog
+{
+    //properties
+    static int legs = 4;
+    String color;
+    String breed;
+
+
+
+}
